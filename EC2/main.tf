c8c1f64e79f0db77171c178d44a7868356029f39
Ubuntu <ubuntu@ip-172-31-42-192.eu-west-1.compute.internal>
2020-08-20 09:26:40 +0000

    updated EC2 main

--- EC2/main.tf
+++ EC2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                         = var.ami
   instance_type               = var.instance
   key_name                    = var.key_name
-  subnet_id                   = aws_subnet.subnet_a.id
+  //subnet_id                   = aws_subnet.subnet_a.id
   associate_public_ip_address = true
-  vpc_security_group_ids      = ["${aws_security_group.ingress-all-test.id}"]
+#   vpc_security_group_ids      = ["${aws_security_group.ingress-all-test.id}"]
 }
